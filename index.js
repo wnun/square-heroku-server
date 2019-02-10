e4fe7ad2cfdf3b484ad5fa093cfa200f06e90123
wnun <46791688+wnun@users.noreply.github.com>
2019-02-10 01:23:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/chargeForCookie', async (request, response) => {
         name: "All Access Membership",
         quantity: "1",
         base_price_money: {
-          amount: 1781,
+          amount: requestBody.amount,
           currency: "USD"
         }
       }
