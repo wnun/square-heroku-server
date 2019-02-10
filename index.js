4a136d965c43b75eb71da6e77b46dd9d1df14b37
wnun <46791688+wnun@users.noreply.github.com>
2019-02-10 02:20:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 9 */
+
 const express = require('express');
 const app = express();
 const SquareConnect = require('square-connect');
@@ -24,8 +26,17 @@ app.post('/chargeForCookie', async (request, response) => {
   const requestBody = request.body;
   const locations = await locationsApi.listLocations();
   const locationId = locations.locations[0].id;
-  
-    const transaction = await transactionsApi.charge(locationId, requestBody);
+  const order = await ordersApi.createOrder(requestBody);
+  try {
+    const chargeBody = {
+      "idempotency_key": crypto.randomBytes(12).toString('hex'),
+      "card_nonce": requestBody.nonce,
+      "amount_money": {
+         ...order.order.total_money
+      },
+      "order_id": order.order.id
+    };
+    const transaction = await transactionsApi.charge(locationId, chargeBody);
     console.log(transaction.transaction);
 
     response.status(200).json(transaction.transaction);
